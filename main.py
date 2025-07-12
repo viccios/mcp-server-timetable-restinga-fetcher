00b7b014f0205bf80f2954e56f76e04d456c4e70
Vinícius Nunes de Oliveira <br.viniciusnunes@gmail.com>
2025-07-12 03:20:10 -0300

    feat: update to the new version of Timetable Restinga Fetcher

--- main.py
+++ main.py
@@ -1,55 +1,88 @@
-from fastmcp import FastMCP
+import os
 import requests
 from requests import Response
+from dotenv import load_dotenv
+from fastmcp import FastMCP
+
+load_dotenv()
+
+TIMETABLE_RESTINGA_FETCHER_API_URL = os.environ["TIMETABLE_RESTINGA_FETCHER_API_URL"]
 
 mcp = FastMCP("Fetch schedule data from Timetable Restinga website")
 
 
 @mcp.tool
-def get_teachers() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/teachers")
+def get_periods() -> dict:
+    """Get the name, ID and start and end time of the periods"""
+    response: Response = requests.get(f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/periods")
     return response.json()
 
 
 @mcp.tool
 def get_subjects() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/subjects")
+    """Get the name and ID of the subjects"""
+    response: Response = requests.get(f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/subjects")
     return response.json()
 
 
 @mcp.tool
-def get_classrooms() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/classrooms")
+def get_teachers() -> dict:
+    """Get the name and ID of the teachers"""
+    response: Response = requests.get(f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/teachers")
     return response.json()
 
 
 @mcp.tool
 def get_classes() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/classes")
+    """Get the name and ID of school subjects"""
+    response: Response = requests.get(f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/classes")
     return response.json()
 
 
 @mcp.tool
-def get_periods() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/periods")
+def get_classrooms() -> dict:
+    """Get the name and ID of the classroooms"""
+    response: Response = requests.get(
+        f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/classrooms"
+    )
     return response.json()
 
 
 @mcp.tool
-def get_dayparts() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/dayparts")
+def get_class_schedule() -> dict:
+    """
+    Get the name and ID of the classes,
+    with detailed schedule information
+    """
+    response: Response = requests.get(
+        f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/class_schedule"
+    )
     return response.json()
 
 
 @mcp.tool
-def get_dates() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/dates")
+def get_teachers_schedule() -> dict:
+    """
+    Get the name and ID of teachers,
+    with detailed information about
+    classes, classrooms and schedules
+    """
+    response: Response = requests.get(
+        f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/teachers_schedule"
+    )
     return response.json()
 
 
 @mcp.tool
-def get_event_types() -> dict:
-    response: Response = requests.get("http://localhost:3000/api/event_types")
+def get_classrooms_schedule() -> dict:
+    """
+    Get the name and ID of the classrooms,
+    with detailed information about
+    classes, teachers, classes and schedules
+    """
+    response: Response = requests.get(
+        f"{TIMETABLE_RESTINGA_FETCHER_API_URL}/classrooms_schedule"
+    )
     return response.json()
 
 
